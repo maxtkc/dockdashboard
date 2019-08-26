d3fbb86643b04589df2355da11cc472d851a7a42
maxtkc <maxkatzchristy@gmail.com>
2019-08-26 16:24:34 -0400

    new clients are synced immediately

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 40510 });
 
+let test_table_data = "[[],[\"Mark\",\"Otto\",\"@mdo\"],[\"Jacob\",\"Thornto\",\"@fat\"],[\"Larry\",\"the Bird\",\"@twitter\"]]";
+
 wss.on('connection', function connection(ws) {
+  ws.send(test_table_data);
   ws.on('message', function incoming(data) {
+    test_table_data = data;
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
